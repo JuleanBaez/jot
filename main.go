40c3cc10de7b2590f9da935e6ef398b0edb1b760
jeweltech <jbaez71602@gmail.com>
2026-03-17 13:56:27 -0400

    Jot V1: CLI Note-taking tool

---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+const (
+	timestampLayout = "2006-01-02 15:04:05"
+	defaultFileName = "/Documents/jot.txt"
+)
+
+func getPath() string {
+
+	// determines the storage location. prioritizes the JOT_PATH and falls back to
+	// a default location in the Documents folder.
+	path := os.Getenv("JOT_PATH")
+	if path == "" {
+		homeDir, err := os.UserHomeDir()
+
+		if err != nil {
+			fmt.Println("Error getting home directory:", err)
+			os.Exit(1)
+		}
+
+		path = homeDir + defaultFileName
+	}
+
+	return path
+}
+
+func userInput(userNote []string) {
+
+	passedNote := strings.Join(userNote, " ")
+
+	formattedTime := time.Now().Format(timestampLayout)
+	finalLine := fmt.Sprintf("[%s] %s\n", formattedTime, passedNote)
+
+	findFile(finalLine)
+}
+
+func findFile(noteText string) {
+
+	filePath := getPath()
+
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	check(err, "Could not open file")
+	defer file.Close()
+
+	_, err = file.WriteString(noteText)
+	check(err, "Error writing note")
+
+	fmt.Println("Note Jotted!")
+}
+
+func viewNote() {
+
+	file, err := os.Open(getPath())
+	check(err, "Failed to get file")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func searchNote(searchTerm string) {
+
+	// scans the jot file for lines containing the searchTerm
+	// and prints matching lines to the terminal.
+	file, err := os.Open(getPath())
+	check(err, "Failed to get file")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		currentLine := scanner.Text()
+
+		if strings.Contains(strings.ToLower(currentLine), strings.ToLower(searchTerm)) {
+			fmt.Println(currentLine)
+		}
+	}
+
+	check(scanner.Err(), "Error reading file")
+}
+
+func deleteNote(searchTerm string) {
+
+	filePath := getPath()
+	tempPath := filePath + ".tmp"
+
+	originalFile, err := os.Open(filePath)
+
+	check(err, "Could not open original file")
+	defer originalFile.Close()
+
+	tempFile, err := os.Create(tempPath)
+	check(err, "Could not create temp file")
+
+	scanner := bufio.NewScanner(originalFile)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// If the line does not contain the search term, write it to the temp file
+		if !strings.Contains(strings.ToLower(line), strings.ToLower(searchTerm)) {
+			tempFile.WriteString(line + "\n")
+		}
+	}
+
+	tempFile.Close()
+	originalFile.Close()
+
+	err = os.Rename(tempPath, filePath)
+	check(err, "Could not rename temp file")
+
+	fmt.Println("Deleted notes matching:", searchTerm)
+
+}
+
+func check(err error, message string) {
+
+	if err != nil {
+		fmt.Printf("%s: %v\n", message, err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+
+	userNote := os.Args[1:]
+
+	if len(userNote) == 0 {
+		fmt.Println("Usage: jot <command> [arguments]")
+		os.Exit(1)
+	}
+
+	switch userNote[0] {
+	case "view":
+		viewNote()
+	case "search":
+		searchTerm := strings.Join(userNote[1:], " ")
+		searchNote(searchTerm)
+	case "delete":
+		if len(userNote) < 2 {
+			fmt.Println("Usage: jot delete <search term>")
+			os.Exit(1)
+		}
+		searchTerm := strings.Join(userNote[1:], " ")
+		deleteNote(searchTerm)
+	default:
+		userInput(userNote)
+	}
+}
